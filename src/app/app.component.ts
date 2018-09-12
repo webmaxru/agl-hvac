41ac114225e0094b6c11b50de97abc87a51268eb
Maxim Salnikov <salnikov@gmail.com>
2018-09-12 15:10:48 -0700

    afb socket added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as AFB from './afb-websocket.service.js';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,50 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'agl-hvac';
+  afb;
+  ws;
+
+  checkband() {
+    let band = 50;
+    this.callbinder('radio', 'band_supported', { band: band });
+  }
+
+  connect() {
+    this.afb = new AFB.AFB('api', 'mysecret');
+    this.ws = new this.afb.ws(this.onopen, this.onabort);
+  }
+
+  onopen() {
+    this.ws.onevent('*', this.gotevent);
+
+    this.callbinder('radio', 'subscribe', { value: 'frequency' });
+    this.callbinder('radio', 'subscribe', { value: 'station_found' });
+  }
+
+  onabort() {
+    this.callbinder('radio', 'unsubscribe', { value: 'frequency' });
+    this.callbinder('radio', 'unsubscribe', { value: 'station_found' });
+  }
+
+  gotevent(obj) {
+    console.log('gotevent:' + JSON.stringify(obj));
+  }
+
+  replyok(obj) {
+    console.log('replyok:' + JSON.stringify(obj));
+  }
+
+  replyerr(obj) {
+    console.log('replyerr:' + JSON.stringify(obj));
+  }
+
+  callbinder(api, verb, query) {
+    console.log('api=' + api + ' verb=' + verb + ' query=' + query);
+
+    this.ws.call(api + '/' + verb, query).then(this.replyok, this.replyerr);
+  }
+
+  constructor() {
+
+  }
 }
